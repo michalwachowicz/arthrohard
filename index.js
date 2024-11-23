3f2fb61416447b16932b5f3e34afee875ff3f084
michalwachowicz <wachowiczmichal1@gmail.com>
2024-11-23 18:41:30 +0100

    Create products section

--- index.js
+++ index.js
@@ -10,3 +10,51 @@ const menu = (() => {
     else menu.classList.remove("hidden");
   });
 })();
+
+const products = (() => {
+  const container = document.querySelector(".products");
+  const select = document.querySelector("#products-count");
+  const btns = [];
+
+  const showModal = (e) => {
+    const btn = e.target;
+    const id = btn.dataset.id;
+  };
+
+  const clear = () => {
+    while (btns.length > 0) {
+      const btn = btns.pop();
+
+      btn.removeEventListener("click", showModal);
+      btn.remove();
+    }
+  };
+
+  const addBtn = ({ id, text }) => {
+    const btn = document.createElement("button");
+
+    btn.type = "button";
+    btn.className = "btn btn-product";
+    btn.textContent = `ID: ${id}`;
+    btn.dataset.id = id;
+    btn.dataset.text = text;
+    btn.addEventListener("click", showModal);
+
+    container.appendChild(btn);
+    btns.push(btn);
+  };
+
+  const generateProducts = async (size) => {
+    const response = await fetch(
+      `https://brandstestowy.smallhost.pl/api/random?pageNumber=1&pageSize=${size}`
+    );
+    const { data } = await response.json();
+    if (!data || !Array.isArray(data)) return;
+
+    clear();
+    data.forEach(addBtn);
+  };
+
+  select.addEventListener("change", (e) => generateProducts(e.target.value));
+  generateProducts(20);
+})();
